85e10262e0b480c19e3c6c3fdecebe7f7c8536ff
Jukka Svahn <web@rahinaa.biz>
2014-03-20 17:02:05 +0200

    Updated header comments.

--- rah_function.php
+++ rah_function.php
@@ -1,15 +1,32 @@
 <?php
 
+/*
+ * rah_function - Flat templates for Textpattern CMS
+ * https://github.com/gocom/rah_function
+ *
+ * Copyright (C) 2014 Jukka Svahn
+ *
+ * This file is part of rah_function.
+ *
+ * rah_function is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_function is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_function. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
- * Rah_function plugin for Textpattern CMS.
+ * Calls the specified PHP function.
  *
- * @author  Jukka Svahn
- * @license GNU GPLv2
- * @link    http://rahforum.biz/plugins/rah_function
- *
- * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU Genral Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * @param  array       $atts  Attributes
+ * @param  string|null $thing Contained statement
+ * @return string User markup
  */
 
 function rah_function($atts, $thing = null)
